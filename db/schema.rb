824c4f33b7167f25989b74fc787f5499816ab173
Amanda Lilley <yoknitting24@gmail.com>
2015-11-18 21:58:49 +0000

    Create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106233141) do
+ActiveRecord::Schema.define(version: 20151118011956) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
