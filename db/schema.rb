c4f0c3f06739e999abce874b8c8e18a4abfa1166
Amanda Lilley <yoknitting24@gmail.com>
2015-12-12 20:33:32 +0000

    Add user data to articles listing page

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,11 @@
 ActiveRecord::Schema.define(version: 20151127182214) do
 
   create_table "articles", force: :cascade do |t|
-    t.string  "title"
-    t.text    "description"
-    t.integer "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
